59ba8c2c7bf61ea8010d677598ca9e5cbbbd3d28
Hemanth <mallahemanthnaidu2001@gmail.com>
2023-07-15 03:13:41 +0530

    removed server

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('primary_database', 'root', '1234', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
